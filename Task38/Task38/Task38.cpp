a2054681d872a68b3b08ce1ad72adf1355b85f94
Damdin Sandakov <69001156+sandakovdam@users.noreply.github.com>
2024-11-22 06:46:43 +0800

    Add files via upload

---
+++ Task38/Task38/Task38.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+const int MOD = 1000000009;
+
+int main() {
+    const int N = 40;
+    int A = 2, B = 3;
+    int dp[N + 1];
+    dp[0] = 1;
+    for (int i = 1; i <= N; i++) {
+        if (i % 13 == 0) {
+            dp[i] = 0;
+        }
+        else {
+            dp[i] = (i - A >= 0 ? dp[i - A] : 0) + (i - B >= 0 ? dp[i - B] : 0);
+            dp[i] %= MOD;
+        }
+    }
+    cout << dp[N] << endl;
+    return 0;
+}
